75fc963e0469be8e2163aca9e02b7e84566051e1
Umesh Pradip Sanap <128315120+umeshsanap@users.noreply.github.com>
2024-07-23 00:02:56 -0700

    Add files via upload
    This JavaScript program generates a unique username. Simply input your name into the prompt, and our code will instantly create a personalized username for you. Try it now!

---
+++ User_Name_Generator.js
@@ -0,0 +1,4 @@
+let user_name = prompt("Enter the full name of user to create an username");
+console.log("Your User name is generate and it looks like bellow.")
+let new_user_name = "@"+user_name+user_name.length
+console.log(new_user_name)
