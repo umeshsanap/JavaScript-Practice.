ca78d075aa0d9986e502b7bbaa21d72b1a8cbf8a
Umesh Pradip Sanap <128315120+umeshsanap@users.noreply.github.com>
2024-12-07 02:12:54 -0800

    Sum Of n Numbers

---
+++ SumOfNumber.js
@@ -0,0 +1,7 @@
+let num = prompt("Enter number till we calculate the sum ")
+let sum = 0;
+for(let i = 0; i<=num; i++)
+{
+    sum = sum +i;
+}
+console.log("Sum of the number from 0 to ",num," is", sum)
