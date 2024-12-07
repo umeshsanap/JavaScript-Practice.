92f5b186a9cbe4eb991535633f6a02f41e18bac1
Umesh Pradip Sanap <128315120+umeshsanap@users.noreply.github.com>
2024-12-07 02:36:57 -0800

    Add files via upload

---
+++ ForOfLoop.js
@@ -0,0 +1,16 @@
+let str = "umeshsanap";
+for(let i of str)
+{
+    if(i == "s")
+    {
+        console.log("i = $")
+    } 
+    else if(i == "a")
+    {
+        console.log("i = @")
+    }
+    else{
+        console.log("i =",i)
+
+    }
+}
